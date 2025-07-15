00d33a498b67a3e246c2aeb5df69b14b9645b801
Jaylan <jaylansaad20@gmail.com>
2025-07-15 11:46:25 +0300

    commit6

--- src/Hooks/State/SimpleState.jsx
+++ src/Hooks/State/SimpleState.jsx
@@ -1,3 +1,16 @@
-function SimpleState() {}
+function SimpleState () {
+    let clicked = 0;
+    const increment = () => {
+        clicked = clicked + 1;
+        console.log(increment);
+    }
+
+    return <div>
+        <div>
+            <button onClick={increment}>Click Me</button>
+        </div>
+        <h3>Clicked {clicked}</h3>
+    </div>
+}
 
 export default SimpleState;
